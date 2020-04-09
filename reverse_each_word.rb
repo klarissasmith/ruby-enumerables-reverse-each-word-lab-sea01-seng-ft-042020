a30e65e2aa8c824db155f9fdea99ff25230ec39f
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-09 23:09:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 def reverse_each_word(string)
  final_array = []
  string_array = string.split
- final_array.each do |word|
+ string_array.each do |word|
   final_array << word.reverse
  end
  final_array.join
