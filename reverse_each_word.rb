c985719a088a800d3cbe2f064034a95a116723bb
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-09 23:01:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 # string = "Hi again, just making sure it's reversed!"
 def reverse_each_word(string)
- string_array = string.split
- reverse_array = []
+ reverse_array = []string_array = string.split
+ 
  string_array.reverse_each |word|
   p word
  end
